27dd71649f066ab3425457d3affbcdaaf902ea10
Eder Minotti <ederminotti5@outlook.com>
2026-01-19 16:02:50 -0300

    ajustando layout para desktop, e alterando configuraçoes css

--- src/Pages/MoviesHome/index.jsx
+++ src/Pages/MoviesHome/index.jsx
@@ -6,19 +6,22 @@ export const MoviesHome = () => {
   const loggeduser = JSON.parse(localStorage.getItem("loggedUser"));
   return (
     <ul className={styles.movieshome}>
-      <li className={styles.usuario}>Usuario: {loggeduser?.name}</li>
+      <div className={styles.container_user}>
+      <li className={styles.icon_user}>{loggeduser?.name[0]}</li>
+      <li className>{loggeduser?.name}</li>
+      </div>
 
-      <Link to={"/auth/liked"}>
-        <li className={styles.likes}>Gostei</li>
-      </Link>
-
-      <Link to={"/auth/disliked"}>
-        <li className={styles.dislikes}>Não Gostei</li>
-      </Link>
-
-      <Link to={"/auth/later"}>
-        <li className={styles.later}>Ver mais Tarde</li>
-      </Link>
+      <div className={styles.container_menu}>
+        <Link to={"/auth/liked"}>
+          <li className={styles.background}>Gostei</li>
+        </Link>
+        <Link to={"/auth/disliked"}>
+          <li className={styles.background}>Não Gostei</li>
+        </Link>
+        <Link to={"/auth/later"}>
+          <li className={styles.background}>Ver mais Tarde</li>
+        </Link>
+      </div>
 
       <BarraNavegar />
     </ul>
